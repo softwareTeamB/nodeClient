279e85c9f1ae4f7ed7fddeab81cc27642695f398
michel <micheldaniel2@hotmail.com>
2017-09-06 23:40:14 +0200

    interface
    begonnen aan het bouwen van de interface

---
+++ website/js/mainHeadScript.js
@@ -0,0 +1,5 @@
+//voeg deze code toe in de <head> van de html pagina
+var print = ' <link rel="stylesheet" type="text/css" href="/css/main.css">'
+
+//print de codes uit in de html pagina
+document.write(print);
